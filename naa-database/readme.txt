8ae224d920878436392dbc43272f0757ac9f47b8
BH1TAN <yxtccty@163.com>
2021-12-09 18:19:19 +0800

    Learning

---
+++ naa-database/readme.txt
@@ -0,0 +1,5 @@
+excels from
+
+https://www-nds.iaea.org/pgaa/iaeapgaa.htm
+https://www-nds.iaea.org/pgaa/databases.htm
+
--- prepWizard.m
+++ prepWizard.m
@@ -2,8 +2,8 @@
 clear;close all;
 
 %% 原子数表 list_sample_element
-file = 'E:\工作区\0资料大全\金矿\金矿成分ICPMS分析-清析-与澳大利亚结果不符\20210409ICPMS检测报告\检测报告.xlsx';
-list_sample_element_origin = readtable(file,'Sheet','质量占比');
+file = 'demo\ore-icp-ms.xlsx';
+list_sample_element_origin = readtable(file,'Sheet','sheet1');
 list_sample_element = list_sample_element_origin(:,[1,2]);
 list_sample_element.Properties.RowNames = list_sample_element.Element;
 disp('Success: Get list_isotope');
@@ -11,7 +11,7 @@ disp('Success: Get list_isotope');
 %% 同位素丰度表 list_abundance
 % 尚未完成使用定制丰度的代码
 % xls from https://www-nds.iaea.org/pgaa/iaeapgaa.htm
-file = 'E:\myDataBase\NAA-database\isotope.xls';
+file = 'naa-database\isotope.xls';
 list_isotope = readtable(file,'Sheet','for import');
 disp('Success: Get list_abundance');
 
@@ -19,7 +19,7 @@ disp('Success: Get list_abundance');
 % output: struct 'xs_ng',example: xs_ng.Ag107;xs_ng.Ag109
 %         first column: energy(MeV)
 %         second column: cross section (b)
-path_ng = 'E:\myDataBase\TINAA-simulation-tool\xs_n_gamma';
+path_ng = 'xs_n_gamma';
 dir1 = dir(path_ng);
 for i = 3:length(dir1)
     thisName = dir1(i).name(1:end-4);
@@ -33,9 +33,19 @@ end
 disp('Success: Get xs_ng');
 
 %% 缓发伽马库 decayGamma
-file = 'E:\myDataBase\NAA-database\decaygammas.xls';
-list_decay = readtable(file,'Sheet','Decay');
+file = 'naa-database\decaygammas.xls';
+list_decay = readtable(file,'Sheet','for import');
+t=list_decay(:,[1,2,3,7]);t=unique(t);
+disp('Following compond nucleus have metastable state(s),see list_meta:');
+list_meta = cell(0,0);
+for i = 2:size(t,1)
+    if t.A(i)==t.A(i-1)
+        disp(t.Isotope{i});
+        list_meta = [list_meta;{t.Isotope{i},t.Halflife(i)}];
+    end
+end
+disp('Success: Get decayGamma database');
 
 %% 保存
 save('package_sample','list_sample_element');
-save('package_nuclear','list_isotope','xs_ng');
+save('package_nuclear','list_isotope','xs_ng','list_decay');
